0f97ab9c3baa89b93407e2b2d937c0b3dc063a6b
Ruchira Wageesha <ruchira.wageesha@gmail.com>
2018-05-31 08:15:55 +0530

    updating

--- index.js
+++ index.js
@@ -11,7 +11,9 @@ var errors = require('errors');
 
 var redis = new Redis(nconf.get('REDIS_URI'));
 
-var durations = ['second', 'day', 'month'];
+var apisDurations = ['second', 'day', 'month'];
+
+var ipsDurations = ['second', 'minute', 'hour', 'day'];
 
 var map = {
   GET: 'find',
@@ -21,24 +23,57 @@ var map = {
   HEAD: 'find'
 };
 
+var ipsLimits = {
+  find: {
+    second: 10,
+    minute: 500,
+    hour: 5000,
+    day: 50000
+  },
+  create: {
+    second: 10,
+    minute: 100,
+    hour: 500,
+    day: 1000
+  }
+};
+
 var action = function (req) {
   var method = req.method;
   return map[method];
 };
 
-var throttleKey = function (token, name, action, duration) {
+var apisThrottleKey = function (token, name, action, duration) {
   return util.format('throttle:%s:%s:%s:%s', token.id, name, action, duration);
 };
 
-var throttleRules = function (token, name, action, at) {
+var apisThrottleRules = function (token, name, action, at) {
   var tier = token.tier;
   var limits = tier.limits[name] || tier.limits['*'] || {};
   limits = limits[action] || limits['*'] || {};
   var rules = [];
-  durations.forEach(function (duration) {
+  apisDurations.forEach(function (duration) {
     rules.push({
       name: duration,
-      key: throttleKey(token, name, action, duration),
+      key: apisThrottleKey(token, name, action, duration),
+      limit: limits[duration],
+      expiry: at.endOf(duration).unix(),
+    });
+  });
+  return rules;
+};
+
+var ipsThrottleKey = function (ip, action, duration) {
+  return util.format('throttle:%s:%s:%s', ip, action, duration);
+};
+
+var ipsThrottleRules = function (ip, action, at) {
+  var limits = ipsLimits[action] || ipsLimits['*'] || {};
+  var rules = [];
+  ipsDurations.forEach(function (duration) {
+    rules.push({
+      name: duration,
+      key: ipsThrottleKey(ip, action, duration),
       limit: limits[duration],
       expiry: at.endOf(duration).unix(),
     });
@@ -59,10 +94,10 @@ var check = function (rules, done) {
   done();
 }
 
-var throttle = function (token, name, action, done) {
+var ips = function (ip, action, done) {
   var at = moment().utc();
-  var rootKey = throttleKey(token, name, action, '');
-  var rules = throttleRules(token, name, action, at);
+  var rootKey = ipsThrottleKey(ip, action, '');
+  var rules = ipsThrottleRules(ip, action, at);
   var multi = redis.multi();
   // primary check
   rules.forEach(function (rule) {
@@ -119,13 +154,88 @@ var throttle = function (token, name, action, done) {
   });
 };
 
-module.exports = function (options) {
+exports.ips = function () {
+  return function (req, res, next) {
+    var ip = req.ip
+    ips(ip, action(req), function (err) {
+      if (!err) {
+        return next();
+      }
+      if (err.code !== errors.tooManyRequests().code) {
+        return next(err);
+      }
+      res.pond(err);
+    });
+  };
+};
+
+var apis = function (token, name, action, done) {
+  var at = moment().utc();
+  var rootKey = apisThrottleKey(token, name, action, '');
+  var rules = apisThrottleRules(token, name, action, at);
+  var multi = redis.multi();
+  // primary check
+  rules.forEach(function (rule) {
+    multi.get(rule.key);
+  });
+  multi.exec(function (err, results) {
+    if (err) {
+      return done(err);
+    }
+    var index = 0;
+    rules.forEach(function (rule) {
+      var entry = results[index++];
+      rule.current = entry[1];
+    });
+    check(rules, function (err) {
+      if (err) {
+        return done(err);
+      }
+      // secondary check
+      multi = redis.multi();
+      rules.forEach(function (rule) {
+        multi.set(rootKey, 0)
+          .expireat(rootKey, rule.expiry)
+          .renamenx(rootKey, rule.key)
+          .incr(rule.key)
+          .ttl(rule.key)
+      });
+      // [[null,"OK"],[null,1],[{}],[null,1],[null,-1],[null,"OK"],[null,1],[null,0],[null,11],[null,72401],[null,"OK"],[null,1],[null,0],[null,11],[null,2059601]]
+      multi.exec(function (err, results) {
+        if (err) {
+          return done(err);
+        }
+        var index = 0;
+        rules.forEach(function (rule) {
+          var entry = results[index += 3];
+          rule.current = entry[1];
+          entry = results[index += 1];
+          rule.ttl = entry[1];
+          index++;
+        });
+        async.each(rules, function (rule, updated) {
+          if (rule.ttl !== -1) {
+            return updated();
+          }
+          redis.expireat(rule.key, rule.expiry, updated);
+        }, function (err) {
+          if (err) {
+            return done(err);
+          }
+          check(rules, done);
+        });
+      });
+    });
+  });
+};
+
+exports.apis = function (name) {
   return function (req, res, next) {
     var token = req.token;
     if (!token) {
       return next();
     }
-    throttle(token, options.name, action(req), function (err) {
+    apis(token, name, action(req), function (err) {
       if (!err) {
         return next();
       }
